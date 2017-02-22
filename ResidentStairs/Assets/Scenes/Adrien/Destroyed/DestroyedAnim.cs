1f736c156f995f41e74ec90f494fa70207615a9d
Adrien <meuseuneu@hotmail.fr>
2017-02-22 09:51:21 +0100

    Modif Bolt (trail, sprite, couleur...)

---
+++ ResidentStairs/Assets/Scenes/Adrien/Destroyed/DestroyedAnim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyedAnim : MonoBehaviour {
+
+    public Material m_material;
+    public ParticleSystem m_particleSystem;
+    
+    public bool state = false;
+
+    float disp = 0.0f;
+    float dispMax = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+            if (!state) m_particleSystem.Emit(30);
+            disp += 0.1f * Time.fixedDeltaTime;
+            if (disp >= dispMax)
+            {
+                disp = 0.0f;
+                state = false;
+            }
+            else state = true;
+
+            m_material.SetFloat("_Displacement", disp);
+
+
+    }
+}
