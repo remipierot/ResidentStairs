9beb4cdcb7e3bc21c1a837b2da853a4d22c6a734
Remi Pierot <rfpierot91@gmail.com>
2017-02-22 12:20:37 +0100

    Enemy death

--- ResidentStairs/Assets/Scenes/Remi/Scripts/EnemyScript.cs
+++ ResidentStairs/Assets/Scenes/Remi/Scripts/EnemyScript.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour {
 	private Rigidbody _Body;
+	private BoxCollider _Collider0;
+	private CapsuleCollider _Collider1;
 
 	public float Speed = 1.0f;
+	public DestroyedAnim KillScript;
+	public Material DyingMaterial;
 
 	// Use this for initialization
 	void Start ()
 	{
 		_Body = GetComponent<Rigidbody>();
+		_Collider0 = GetComponent<BoxCollider>();
+		_Collider1 = GetComponent<CapsuleCollider>();
 	}
 
 	private void Update()
@@ -18,8 +24,23 @@ public class EnemyScript : MonoBehaviour {
 		_Body.AddForce(-transform.up * Speed);
 	}
 
-	void OnTriggerEnter(Collider other)
+	private void OnTriggerEnter(Collider other)
 	{
-		//Destroy(gameObject);
+		if(other.CompareTag("Shots") || other.CompareTag("Player"))
+		{
+			Die();
+		}
+	}
+
+	private void Die()
+	{
+		Material dying = new Material(DyingMaterial);
+		dying.SetFloat("_Displacement", 0);
+		GetComponent<MeshRenderer>().material = dying;
+		_Collider0.enabled = false;
+		_Collider1.enabled = false;
+
+		KillScript.enabled = true;
+		KillScript.SetMaterial(dying);
 	}
 }
