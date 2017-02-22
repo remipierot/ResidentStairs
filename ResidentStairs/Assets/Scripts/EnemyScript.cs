3c4bac649667978c3f60bb5b6c2ac0197850ecc0
Remi Pierot <rfpierot91@gmail.com>
2017-02-22 09:18:15 +0100

    First enemy test

---
+++ ResidentStairs/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+	private Rigidbody _Body;
+
+	public float Speed = 1.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_Body = GetComponent<Rigidbody>();
+	}
+
+	private void Update()
+	{
+		_Body.AddForce(transform.forward * Speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		//Destroy(gameObject);
+	}
+}
---
+++ ResidentStairs/Assets/Scripts/Halftone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Halftone : MonoBehaviour
+{
+	private Material material;
+
+	void Awake()
+	{
+		material = new Material(Shader.Find("Hidden/HalftoneShader")); ;
+	}
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		float elapsedTime = material.GetFloat("_ElapsedTime") + Time.deltaTime;
+		material.SetFloat("_ElapsedTime", elapsedTime);
+		material.SetTexture("_MainTex", source);
+		Graphics.Blit(source, destination, material);
+	}
+}
