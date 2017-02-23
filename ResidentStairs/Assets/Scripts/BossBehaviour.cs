0f3402ba6181f1f6105f9ffafe822600c6f69c96
Adrien <meuseuneu@hotmail.fr>
2017-02-23 12:12:00 +0100

    Boss explosion quand mort.

--- ResidentStairs/Assets/Scripts/BossBehaviour.cs
+++ ResidentStairs/Assets/Scripts/BossBehaviour.cs
@@ -9,42 +9,68 @@ public class BossBehaviour : MonoBehaviour {
 
     public Transform m_transform;
     public GameObject m_hitParticleSystem;
+    public GameObject m_meshContainer;
 
+    public Vector3 midPos;
     public Vector3 boundUp;
     public Vector3 boundDown;
     public float speed;
     bool goingUp = true;
+    bool alive = true;
+    bool arriving = true;
+
+    public ParticleSystem m_deathParticle;
 
 	// Use this for initialization
 	void Start () {
         m_transform = GetComponent<Transform>();
+        arriving = true;
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
-        if(goingUp)
+        if(arriving)
         {
-            m_transform.position = Vector3.MoveTowards(m_transform.position, boundUp, speed * Time.deltaTime);
-            if (m_transform.position.y >= boundUp.y) goingUp = false;
-        }
-        else
-        {
-            m_transform.position = Vector3.MoveTowards(m_transform.position, boundDown, speed * Time.deltaTime);
-            if (m_transform.position.y <= boundDown.y) goingUp = true;
+            m_transform.position = Vector3.MoveTowards(m_transform.position, midPos, speed * Time.deltaTime);
+            if (m_transform.position.z <= midPos.z)
+            {
+                arriving = false;
+                alive = true;
+            }
         }
 
-	}
+        if(alive)
+        {
+            if(goingUp)
+            {
+                m_transform.position = Vector3.MoveTowards(m_transform.position, boundUp, speed * Time.deltaTime);
+                if (m_transform.position.y >= boundUp.y) goingUp = false;
+            }
+            else
+            {
+                m_transform.position = Vector3.MoveTowards(m_transform.position, boundDown, speed * Time.deltaTime);
+                if (m_transform.position.y <= boundDown.y) goingUp = true;
+            }
+
+        }
+
+    }
 
     public void takeHit(int hit)
     {
-        life -= hit;
-        m_hitParticleSystem.GetComponent<BossHitParticle>().hit = true;
-        if (life == 0) Die();
+        if (alive)
+        {
+            life -= hit;
+            m_hitParticleSystem.GetComponent<BossHitParticle>().hit = true;
+            if (life <= 0) Die();
+        }
     }
 
     void Die()
     {
-        // DIE DIE DIE
+        alive = false;
+        m_deathParticle.Emit(10000);
+        m_meshContainer.SetActive(false);
     }
 }
